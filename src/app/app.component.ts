4cde2d11c9865455f10091ad532f66aeeb556f2a
vinay <vinay@gmail.com>
2021-03-15 11:30:57 +0530

    all changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import{FormControl,FormGroup,Validators} from  '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular10';
+  loginForm=new FormGroup({
+  username : new FormControl('',Validators.required),
+  email : new FormControl('',Validators.required),
+  password : new FormControl('',Validators.required)
+  })
+  onSubmit(){
+    console.warn(this.loginForm.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
+import{ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
